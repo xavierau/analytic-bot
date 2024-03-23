51239cfec559c5989eb769b199c369ce7b2d1198
Xavier Au <xavier.au@gmail.com>
2024-03-23 22:13:19 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -27,7 +27,7 @@ function convertMarkdownToHtml(markdown) {
 
     console.log(doc)
     console.log(doc.body)
-    console.log(doc.body.innerHTML || "empty")
+    console.log(doc.body?.innerHTML || "empty")
 
     return doc
 }
